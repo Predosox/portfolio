8e607e3f7db1fdd697dca6a3a1cdc6b7d2a75ed2
Arthur Almeida Pedroso <arthuralmeidapedoso3@gmail.com>
2025-03-04 17:26:24 -0300

    Melhora da estética

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ function mostrarProjetos(tipo) {
     let botoes = document.querySelectorAll(".aba");
 
     if (tipo === "grandes") {
-        projetosGrandes.style.display = "block";
-        projetosMenores.style.display = "none";
-    } else if (tipo === "menores") {
-        projetosGrandes.style.display = "none";
-        projetosMenores.style.display = "block";
+        projetosGrandes.style.opacity = "0";
+        setTimeout(() => {
+            projetosGrandes.style.display = "flex";
+            projetosMenores.style.display = "none";
+            projetosGrandes.style.opacity = "1";
+        }, 300);
+    } else {
+        projetosMenores.style.opacity = "0";
+        setTimeout(() => {
+            projetosMenores.style.display = "flex";
+            projetosGrandes.style.display = "none";
+            projetosMenores.style.opacity = "1";
+        }, 300);
     }
 
-    
+    // Muda a aba ativa
     botoes.forEach(botao => botao.classList.remove("ativa"));
     document.querySelector(`button[onclick="mostrarProjetos('${tipo}')"]`).classList.add("ativa");
 }
